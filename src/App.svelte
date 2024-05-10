3102dce396bfea6a8c18842ec9a0c1286eb31611
Iva Stolnik <iva.stolnik91@gmail.com>
2024-05-10 17:34:31 +0200

    add callService and callWS examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,62 @@
 <script>
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+
     export let config = {};
     export let hass = {};
+
+    let configIn = writable({});
+    let haStatus = writable('');
+
+    // Method to call a service
+    function callService(domain, service, serviceData) {
+        if (hass && hass.callService) {
+            hass.callService(domain, service, serviceData);
+        } else {
+            console.error("Home Assistant instance (hass) or callService method is not available.");
+        }
+    }
+
+    // Method to call a WebSocket
+    function callWebSocket(type, domain, service, serviceData) {
+        if (hass && hass.callWS) {
+            const message = {
+                type: type,
+                domain: domain,
+                service: service,
+                service_data: serviceData
+            };
+            hass.callWS(message);
+        } else {
+            console.error("Home Assistant instance (hass) or callWS method is not available.");
+        }
+    }
+
+    onMount(() => {
+        if (config) {
+            configIn.set(config);
+        }
+
+        if (hass) {
+            haStatus.set('HA Connected');
+        } else {
+            haStatus.set('No HA Connection');
+        }
+    });
 </script>
 
 <div>
-    <p>Configuration: {JSON.stringify(config)}</p>
-    <p>HASS: {JSON.stringify(hass)}</p>
+    <h1>Configuration</h1>
+    <pre>{$configIn}</pre>
+    <h2>Status: {$haStatus}</h2>
+    <div>
+        <button on:click={() => callService('light', 'toggle', { entity_id: 'light.wl_office_1'})}>
+            Toggle Light (call service)
+        </button>
+    </div>
+    <div>
+        <button on:click={() => callWebSocket('call_service', 'light', 'toggle', { entity_id: 'light.wl_office_1' })}>
+            Toggle Light (call websocket)
+        </button>
+    </div>
 </div>
